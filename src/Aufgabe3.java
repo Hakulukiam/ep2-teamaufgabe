0ea2bd5f8c6d69bd491c0171e64bc031ad341d2c
Hakulukiam <markus.ruckenbauer@kinamu.com>
2018-05-03 09:23:46 +0200

    updated test

--- src/Aufgabe3.java
+++ src/Aufgabe3.java
@@ -9,13 +9,16 @@ public class Aufgabe3 {
     public static void main(String[] args) {
         //Change only these Variables
         String path = "/data/junctions.csv";
-        int testsize = 100;
+        int testsize = 1000;
         int test = 0;
         //vars End
 
         long start;
         long ALtime;
         long QTtime;
+        double ALTotal = 0.0;
+        double QTTotal = 0.0;
+
         DataStructureArrayList ALdata = new DataStructureArrayList(path);
         DataStructureQuadTree QTdata = new DataStructureQuadTree(path);
 
@@ -23,12 +26,19 @@ public class Aufgabe3 {
             start = System.nanoTime();
             new Umkreissuche(ALdata, test);
             ALtime = System.nanoTime() - start;
+            ALTotal += ALtime/testsize;
 
             start = System.nanoTime();
             new Umkreissuche(QTdata, test);
             QTtime = System.nanoTime() - start;
+            QTTotal += QTtime/testsize;
 
             System.out.println("[" + i + "] AL: " + ALtime + " --- QT: " + QTtime);
         }
+
+        System.out.println("Total Times:");
+        System.out.println("AL: "+ALTotal+" QT: "+QTTotal);
+        double difference =  ((ALTotal/QTTotal)*100)-100;
+        System.out.println("The Implementations show a difference of "+String.format("%.2f", difference)+"%");
     }
 }
